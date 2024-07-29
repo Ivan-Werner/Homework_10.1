72f3c2fd1f686fa2f2e3e3c0e333ddacf2a5fe10
Ivan_K <ikalinov1989yandex.ru>
2024-07-29 20:32:44 +0400

    Add func mask_account_card + get_date

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,24 @@
+from masks import get_mask_card_number, get_mask_account
 
+
+def mask_account_card(bank_info):
+    number = ''
+    text = ''
+    for i in bank_info:
+        if i.isdigit():
+            number += i
+        else:
+            text += i
+    if len(number) == 16:
+        result = get_mask_card_number(number)
+    elif len(number) == 20:
+        result = get_mask_account(number)
+    return text + result
+
+
+def get_date(date: str) -> str:
+    date_cut = date[:10].split('-')
+    result = '.'.join(date_cut[::-1])
+    return result
+
+print(mask_account_card('Счет 35383033474447895560'))
