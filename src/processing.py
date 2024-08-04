a9c189f04a37a1f8a498a4283a442251dd6338ca
Ivan_K <ikalinov1989yandex.ru>
2024-08-04 18:40:55 +0400

    Add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -5,4 +5,9 @@ def filter_by_state(operations_list: list, state='EXECUTED') -> list:
         for k, v in operation.items():
             if v == state:
                 res.append(operation)
-    return
+    return res
+
+def sort_by_date(operations_list: list, des_order=True):
+    """Сортирует список словарей по полю 'дата' по убыванию или по возрастанию"""
+    sorted_list = sorted(operations_list, key=lambda k: k['date'], reverse=des_order)
+    return sorted_list
