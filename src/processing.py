4c529b4517621682f2138cc4ad19156efb45da8f
Ivan_K <ikalinov1989yandex.ru>
2024-08-04 18:00:52 +0400

    Add function filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(operations_list: list, state='EXECUTED') -> list:
+    """Возвращает список словарей, ключ которых соответствует указанному значению"""
+    res = []
+    for operation in operations_list:
+        for k, v in operation.items():
+            if v == state:
+                res.append(operation)
+    return res
