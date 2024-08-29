ae6c223cb1c2cf7dc05fe838f46ad17048b7804b
Ivan_K <ikalinov1989yandex.ru>
2024-08-29 21:06:20 +0400

    Homework 12.2 for checking

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,40 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("..\\logs\\masks.log", mode='w+')
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s : %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+logger.info("Запрос номера карты")
+
+
 def get_mask_card_number(card_num: str) -> str:
     """Возвращает маску номера карты по правилу по правилу XXXX XX** **** XXXX"""
+
     if len(card_num) == 16 and card_num.isdigit():
+        logger.info("Вывод маскированного номера карты")
         return card_num[:4] + " " + card_num[4:6] + "** **** " + card_num[-4:]
     else:
+        logger.error(f"Неверный номер карты {card_num}")
         return 'Неверный формат номера карты'
 
 
+logger.info("Запрос номера счета")
+
+
 def get_mask_account(bill_num: str) -> str:
-    """Возвращает маску номера по правилу **XXXX"""
+    """Возвращает маску номера счета по правилу **XXXX"""
     if len(bill_num) == 20 and bill_num.isdigit():
+        logger.info("Вывод маскированного номера счета")
         return "**" + bill_num[-4:]
     else:
+        logger.error(f"Неверный номер счета {bill_num}")
         return 'Неверный формат номера счета'
 
 
 if __name__ == '__main__':
-    print(get_mask_card_number('7000792289606361'))
-    print(get_mask_account('73654108430135874305'))
+    print(get_mask_card_number('7000792289606361'))  #7000792289606361
+    print(get_mask_account('73654108430135874305'))  #73654108430135874305
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,33 @@
+import logging
 import os
 import json
 from config import DATA_DIR
 
 operations_path = os.path.join(DATA_DIR, "operations.json")
 
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("..\\logs\\utils.log", mode='w+')
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s : %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 def operations_data(operations_path: str) -> list:
     """Функция возвращает список словарей с данными о финансовых транзакциях из JSON-файла"""
     try:
+        logger.info("Открываем JSON-файл")
         with open(operations_path, encoding='utf-8') as f:
             data = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("Ошибка: файл не найден или файл пустой")
         data = []
 
     if data == [] or type(data) != list:
+        logger.error("JSON - файл является пустым списком или не является списком")
         return []
     else:
+        logger.info("Вывод данных о транзакциях")
         return data
 
 
